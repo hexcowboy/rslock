f92d45a0f489d0dfe4de6933c4cca2577409c503
Henry Zimmerman <hzimmerm@akamai.com>
2023-08-25 14:30:18 -0400

    Do not include LockGuard and dependent functions if tokio-comp is enabled

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 mod lock;
 
-pub use crate::lock::{Lock, LockError, LockGuard, LockManager};
+#[cfg(any(feature = "async-std-comp", feature = "tokio-comp"))]
+pub use crate::lock::{Lock, LockError, LockManager};
+#[cfg(all(feature = "async-std-comp", not(feature = "tokio-comp")))]
+pub use crate::lock::LockGuard;
