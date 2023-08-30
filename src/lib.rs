4de6748d0910235efd44ef240c8ac19ba5efef19
Henry Zimmerman <hgzimmerman@users.noreply.github.com>
2023-08-30 19:59:46 -0400

    Control if redis uses async-std or tokio via feature flags (#4)
    * Control if redis uses async-std or tokio via feature flags
    
    * Do not include LockGuard and dependent functions if tokio-comp is enabled
    
    * make tests pass with cargo test --features tokio-comp
    
    * add doc comments; promote comments to doc comments; alter cfg conditions
    
    * only compile the lock module if either of the async runtime flags are present
    
    * test raii unlocking only when only async-std is enabled
    
    * add default-features test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#[cfg(any(feature = "async-std-comp", feature = "tokio-comp"))]
 mod lock;
 
+#[cfg(any(feature = "async-std-comp", feature = "tokio-comp"))]
 pub use crate::lock::{Lock, LockError, LockGuard, LockManager};
